29f00b525009990a806f8a3824d3e35746b4d09b
DavidHunterScot <david.hunter@echoverse.cc>
2023-08-02 00:00:23 +0100

    Added a line of text to the about page.

--- src/about.php
+++ src/about.php
@@ -10,7 +10,7 @@ footer_scripts: /assets/javascript/anti-email-harvesting.js
 
 <h2>What is DAH5?</h2>
 <p>DAH5 is a personal internet alias of mine, created back in 2005 for exploring the technologies that make up the world wide web, and for working on some personal projects.</p>
-<p>Today, the DAH5 name represents a long history of my personal achievements, and my continued evolution online.</p>
+<p>Today, the DAH5 name represents a long history of my personal achievements, and my continued evolution online. It's the website that started it all!</p>
 
 <h2>Technologies Behind the Website</h2>
 <p>Developed using my own Static Site Generater called <a href="https://github.com/DavidHunterScot/StaticPHP" target="_blank">StaticPHP</a>, and hosted by <a href="https://pages.cloudflare.com" target="_blank">CloudFlare Pages</a>. The source code is available on <a href="https://github.com/DavidHunterScot/DAH5" target="_blank">GitHub</a>.</p>
