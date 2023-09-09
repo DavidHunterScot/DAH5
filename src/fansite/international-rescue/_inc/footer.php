99cb819f2e5059a24819bc0de7f6a95770334f20
DavidHunterScot <david.hunter@echoverse.cc>
2023-09-09 22:47:58 +0100

    Added footer note to International Rescue Fan Site.

--- src/fansite/international-rescue/_inc/footer.php
+++ src/fansite/international-rescue/_inc/footer.php
@@ -1,3 +1,6 @@
+    <div class="footer">
+        <p>This is a fan-made website related to the International Rescue orgamisation from the original Thunderbirds TV show. Copyright belongs to their respective holders.</p>
+    </div>
 </div>
 
 <?php include 'src/_inc/footer.php'; ?>
