cab5c77fcc9d5bd15c6ac7201c06f160b759926b
DavidHunterScot <david.hunter@echoverse.cc>
2023-09-11 00:00:00 +0100

    Improved the A-Z page.

--- src/a-z.php
+++ src/a-z.php
@@ -8,7 +8,43 @@ layout: src/_inc/base.php
 <p>This page has a complete list of all pages on the DAH5 website. It may also include pages in development with broken features.</p>
 
 <ul class="sitemap">
---- loop( dir = "src", ignores = "404;staticphp" ) ---
-    <li><a href="--- loop.uri ---">--- loop.page_title ---</a></li>
---- endloop ---
+    <li><a href="/about">About</a></li>
+    <li><a href="/favourite-actors-and-actresses">Favourite Actors and Actresses</a></li>
+    <li><a href="/games">Games</a></li>
+    <li><a href="/">Home</a></li>
+    <li><a href="/reference">Reference</a></li>
+    <li><a href="/staticphp">StaticPHP</a></li>
+    <li><a href="/tools">Tools</a></li>
 </ul>
+
+<div class="sitemaps">
+    <ul class="sitemap">
+        <li><a href="/games">Games</a></li>
+        <li><a href="/games/love-calculator">Love Calculator</a></li>
+    </ul>
+
+    <ul class="sitemap">
+        <li><a href="/reference">Reference</a></li>
+        <li><a href="/reference/html">HTML</a></li>
+    </ul>
+
+    <ul class="sitemap">
+        <li><a href="/staticphp">StaticPHP</a></li>
+        <li><a href="/staticphp/about">About</a></li>
+        <li><a href="/staticphp/docs">Docs</a></li>
+    </ul>
+
+    <ul class="sitemap">
+        <li><a href="/staticphp/docs">StaticPHP Docs</a></li>
+        <li><a href="/staticphp/docs/functional-blocks">Functional Blocks</a></li>
+        <li><a href="/staticphp/docs/getting-started">Getting Started</a></li>
+        <li><a href="/staticphp/docs/html-files">HTML Files</a></li>
+        <li><a href="/staticphp/docs/metadata">MetaData</a></li>
+        <li><a href="/staticphp/docs/php-files">PHP Files</a></li>
+    </ul>
+
+    <ul class="sitemap">
+        <li><a href="/tools">Tools</a></li>
+        <li><a href="/tools/find-urls-in-string">Find URLs in String</a></li>
+    </ul>
+</div>
