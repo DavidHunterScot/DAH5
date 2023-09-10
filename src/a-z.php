b94342eaa2712ab90bc477772c38d24979dafc13
DavidHunterScot <david.hunter@echoverse.cc>
2023-09-10 20:09:44 +0100

    Started merging the StaticPHP into DAH5

--- src/a-z.php
+++ src/a-z.php
@@ -8,7 +8,7 @@ layout: src/_inc/base.php
 <p>This page has a complete list of all pages on the DAH5 website. It may also include pages in development with broken features.</p>
 
 <ul class="sitemap">
---- loop( dir = "src", ignores = "404" ) ---
+--- loop( dir = "src", ignores = "404;staticphp" ) ---
     <li><a href="--- loop.uri ---">--- loop.page_title ---</a></li>
 --- endloop ---
 </ul>
