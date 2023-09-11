c1929505596eeb59d2c8793d4aabb0d45fc1d8bd
DavidHunterScot <david.hunter@echoverse.cc>
2023-09-11 18:36:08 +0100

    Moved StaticPHP image assets to relative storage.

--- src/staticphp/index.php
+++ src/staticphp/index.php
@@ -3,7 +3,7 @@ staticphp_nav_item: home
 layout: src/staticphp/_includes/base.php
 ---
 
-<p><img src="--- metadata.images_url ---/logos/StaticPHP-Promo1.png" alt="Static PHP Promo Image"></p>
+<p><img src="--- metadata.images_url ---/StaticPHP-Promo1.png" alt="Static PHP Promo Image"></p>
 <h2>Welcome to --- metadata.site_title ---!</h2>
 <p>--- metadata.site_tagline ---</p>
 <p><b>NEW:</b> Now supports <a href="/staticphp/docs/html-files">HTML files</a>!</p>
