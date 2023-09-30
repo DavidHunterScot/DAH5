8cc3fb19db563a3c0520febf2c0cfa42432ffbad
DavidHunterScot <david.hunter@echoverse.cc>
2023-09-30 17:07:53 +0100

    Improved the DAH5 Homepage

--- src/index.php
+++ src/index.php
@@ -4,8 +4,29 @@ current_nav_item: home
 layout: src/_inc/base.php
 ---
 
-<h1>Welcome to DAH5!</h1>
+<div class="dah5-homepage">
+    <h1>Welcome to DAH5!</h1>
 
-<p>This website is still undergoing heavy development work. Some features may come and go.</p>
-<p>Feel free to have a look at anything that is currently available, or come back later to see how things have progressed.</p>
-<p>Thank you so much for stopping by.</p>
+    <div class="sections">
+        <div class="section">
+            <div class="img" style="background-image: url( '<?php echo IMAGES_URL; ?>/analogueradio/bbc-radio-4/BBC-Radio-4---198kHz-LW---15-Jan-2021---Midnight-GMT.png' );"></div>
+            <h2>Analogue Radio</h2>
+            <p>An archive of clips to remember how radio sounds over time.</p>
+            <p><a href="/analogueradio" class="button">Visit Site</a></p>
+        </div>
+
+        <div class="section">
+            <div class="img" style="background-image: url( '<?php echo IMAGES_URL; ?>/logos/StaticPHP-Promo1.png' );"></div>
+            <h2>StaticPHP</h2>
+            <p>A Static Site Generator written in PHP. It turns source PHP files into static HTML files.</p>
+            <p><a href="/staticphp" class="button">Visit Site</a></p>
+        </div>
+
+        <div class="section">
+            <div class="img hide-small" style="background-image: url( '<?php echo IMAGES_URL; ?>/ui/transparent-16-9.png' );"></div>
+            <h2>Staticly</h2>
+            <p>The essential repository for Front End Web Developers.</p>
+            <p><a href="/staticly" class="button">Visit Site</a></p>
+        </div>
+    </div>
+</div>
