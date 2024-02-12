b466c9051930ef3635ae8673990794ca477f933a
DavidHunterScot <david.hunter@echoverse.cc>
2024-02-12 23:49:25 +0000

    Fixed Kids Land logo width issue.

--- src/kidsland/index.php
+++ src/kidsland/index.php
@@ -4,7 +4,7 @@ layout: src/kidsland/_inc/base.php
 
 <p class="w3-center w3-xxxlarge w3-text-light-gray"><b>Coming Soon</b></p>
 
-<p class="w3-center"><img src="/kidsland/assets/images/DAH5-KidsLand-Logo-Transparent-Final.png" alt="DAH5 Kids Land logo" class="w3-image" style="max-width: 600px;"></p>
+<p class="w3-center"><img src="/kidsland/assets/images/DAH5-KidsLand-Logo-Transparent-Final.png" alt="DAH5 Kids Land logo" class="w3-image" style="max-width: 600px; width: 100%;"></p>
 
 <p class="w3-center w3-xxxlarge w3-text-light-gray"><b>Coming Soon</b></p>
 
